004b6194ca593c995b6add954a36d133e51f635f
desk11 <lead1@hypersoft.tech>
2025-09-17 10:55:32 +0500

    Stories Sample - BackUp

--- app/src/main/java/com/orbitalsonic/storiessample/domain/useCases/UseCaseStories.kt
+++ app/src/main/java/com/orbitalsonic/storiessample/domain/useCases/UseCaseStories.kt
@@ -1,9 +1,9 @@
 package com.orbitalsonic.storiessample.domain.useCases
 
 import com.orbitalsonic.storiessample.data.entities.ItemStory
-import com.orbitalsonic.storiessample.domain.repositories.RepositoryStories
+import com.orbitalsonic.storiessample.data.repositories.RepositoryStoriesImpl
 
-class UseCaseStories(private val repository: RepositoryStories) {
+class UseCaseStories(private val repository: RepositoryStoriesImpl) {
 
     suspend fun getStories(): List<ItemStory> = repository.getStories()
 
