1fa453c4bf1c643a7cb30323796ec61da4b4587c
desk11 <lead1@hypersoft.tech>
2025-10-18 17:11:53 +0500

    Stories Sample - Completed Functionality

---
+++ app/src/main/java/com/orbitalsonic/storiessample/data/dataSources/entities/StoriesResponse.kt
@@ -0,0 +1,17 @@
+package com.orbitalsonic.storiessample.data.dataSources.entities
+
+data class StoriesResponse(
+    val categories: List<ItemStory>
+)
+
+// Data classes to match the JSON structure in assets/stories.json
+data class StoryItem(
+    val imageUrl: String
+)
+
+data class StoryCategory(
+    val category: String,
+    val subHeader: String,
+    val imageUrl: String,
+    val stories: List<StoryItem>
+)
